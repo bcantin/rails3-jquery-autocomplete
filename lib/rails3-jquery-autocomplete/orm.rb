071b95603f58d2ffcc0fe7c1fdd1f981b2998391
Brad Cantin <bradc@sunbelt-software.com>
2011-07-28 15:18:33 -0500

    forcing mongoid to be loaded

--- lib/rails3-jquery-autocomplete/orm.rb
+++ lib/rails3-jquery-autocomplete/orm.rb
@@ -1,8 +1,6 @@
 module Rails3JQueryAutocomplete
   module Orm
-    autoload :ActiveRecord , 'rails3-jquery-autocomplete/orm/active_record'
 		autoload :Mongoid      , 'rails3-jquery-autocomplete/orm/mongoid'
-		autoload :MongoMapper  , 'rails3-jquery-autocomplete/orm/mongo_mapper'
   end
 end
 
